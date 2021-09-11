9bd99ea582ef5148baf25eedcf44e5c1b45e5dae
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-09-11 23:25:57 +0530

    ADDED : Welcome message

---
+++ src/test/java/com/bridgelabz/HotelReservationTest.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Test;
+
+public class HotelReservationTest {
+
+    @Test
+    void whenHotelAdded_ShouldReturnTrue() {
+        System.out.println("Welcome to Hotel Reservation Program");
+    }
+}
