320b42875a831b9a9da79c9e83a11cb855655bd2
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-09-12 00:22:31 +0530

    ADDED : Hotel Details and List to contain Details

---
+++ src/main/java/com/bridgelabz/HotelService.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HotelService {
+    static List<Hotels> hotelsList = new ArrayList<>();
+
+    boolean addHotelToList(String hotelName, int regularRatePerDay) {
+        Hotels hotels = new Hotels(hotelName, regularRatePerDay);
+        return hotelsList.add(hotels);
+    }
+}
--- src/test/java/com/bridgelabz/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/HotelReservationTest.java
@@ -1,11 +1,22 @@
 package com.bridgelabz;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class HotelReservationTest {
 
     @Test
-    void whenHotelAdded_ShouldReturnTrue() {
+    void givenHotelOne_WhenAdded_ShouldReturnTrue() {
         System.out.println("Welcome to Hotel Reservation Program");
+        HotelService hotelService = new HotelService();
+        try {
+            boolean hotelOne = hotelService.addHotelToList("Lakewood", 110);
+            boolean hotelTwo = hotelService.addHotelToList("Bridgewood", 160);
+            boolean hotelThree = hotelService.addHotelToList("Ridgewood", 220);
+            System.out.println(HotelService.hotelsList);
+            Assertions.assertTrue(hotelOne);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
